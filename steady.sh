c17708b3b6e26294d0458ec212d1ae557d89c017
shereyderam <sororoshervin@gmail.com>
2016-07-17 19:08:18 +0430

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -1,67 +1,3 @@
-#!/bin/bash
-# =====================================================================================================
-# Copyright (C) steady.sh v1.2 2016 iicc (@iicc1)
-# =====================================================================================================
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# this program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# =======================================================================================================
-# It depends on Tmux https://github.com/tmux/tmux which is BSD-licensed
-# and Screen https://www.gnu.org/software/screen GNU-licensed.
-# =======================================================================================================
-# This script is intended to control the state of a telegram-cli telegram bot running in background.
-# The idea is to get the bot fully operative all the time without any supervision by the user.
-# It should be able to recover the telegram bot in any case telegram-cli crashes, freezes or whatever.
-# This script works by tracing ctxt swithes value in kernel procces at a $RELOADTIME 
-# So it can detect any kind of kernel interruption with the procces and reload the bot.
-#
-#--------------------------------------------------
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒█▒▒▒█▒                                --
-#--    ▒▒██▒▒█▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒▒██▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒▒▒█▒▒                                --
-#--    ▒▒▒▒██▒▒▒                                --
-#--    ▒▒▒▒▒▒█▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --                
-#--                                              --                   
-#--------------------------------------------------
-#--                                              --
-#--       Developers: @CRUEL                     --
-#--                                              --
-#--                                              --
-#--------------------------------------------------
-
-
-# Some script variables
 OK=0
 BAD=0
 NONVOLUNTARY=1
@@ -69,8 +5,8 @@ NONVOLUNTARYCHECK=0
 VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
-BOT=wwwwxxxx  # You can put here other bots. Also you can change it to run more than one bot in the same server.
-RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
+BOT=botname  # You can put here other bots. Also you can change it to run more than one bot in the same server.
+RELOADTIME=5  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
 function tmux_mode {
@@ -95,8 +31,8 @@ cat << EOF
  $f1▀ ▀▄▄ ▄▄▀ ▀   $f2 ▀█▄ ▀▀ ▄█▀    $f3▀▄    ▄▀   $f4▀ ▀▄▄ ▄▄▀ ▀   $f5 ▀█▄ ▀▀ ▄█▀    $f6▀▄    ▄▀$rst
  
 EOF
-echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by CRUEL M.KH                 \e[00;37;40m"
+echo -e "               \e[100m                Steady script            \e[00;37;40m"
+echo -e "               \e[01;34m            by shield team              \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -111,7 +47,7 @@ sleep 1.2
 # Checking if the bot folder is in HOME
 echo -e "$bld$f4 CHECKING INSTALLED BOT...$rst"
 sleep 0.5
-ls ../ | grep $BOT > /dev/null
+ls ../ | grep $BOT 2>/dev/null >/dev/null
 if [ $? != 0 ]; then
   echo -e "$f1 ERROR: BOT: $BOT NOT FOUND IN YOUR HOME DIRECTORY$rst"
   sleep 4
@@ -122,11 +58,11 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m                                              \033[0;00m"
+echo -e "\033[38;5;208m             shield team                      \033[0;00m"
+echo -e "\033[38;5;208m             @shieldTM                        \033[0;00m"
+echo -e "\033[38;5;208m          developer : @hafez1116hafez         \033[0;00m"
+echo -e "\033[38;5;208m                                              \033[0;00m"
 
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -142,7 +78,7 @@ sleep 0.9
 # Opening new tmux in a daemon
 echo -e "$bld$f4 ATTACHING TMUX AS DAEMON...$rst"
 # It is recommended to clear cli status always before starting the bot
-rm ../.telegram-cli/state  > /dev/null 
+rm ../.telegram-cli/state 2>/dev/null
 # Nested TMUX sessions trick 
 TMUX= tmux new-session -d -s $BOT "./launch.sh"
 sleep 1.3
@@ -174,7 +110,7 @@ while true; do
 		if [ $I -ge 3 ]; then
 			kill $CLIPID
 			tmux kill-session -t $BOT
-			rm ../.telegram-cli/state  > /dev/null 
+			rm ../.telegram-cli/state 2>/dev/null
 			NONVOLUNTARY=0
 			NONVOLUNTARYCHECK=0
 			VOLUNTARY=0
@@ -195,7 +131,7 @@ while true; do
 		BAD=$(( $BAD + 1 ))
 		sleep 1
 		
-		rm ../.telegram-cli/state  > /dev/null 
+		rm ../.telegram-cli/state 2>/dev/null 
 
 		kill $CLIPID
 		tmux kill-session -t $BOT
@@ -212,6 +148,12 @@ while true; do
 			exit 1
 		fi
 
+	fi
+	
+	# Clear cache after 10h
+	if [ "$OK" == 2400 ]; then
+		sync
+		sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 	fi
 	
 	VOLUNTARY=`echo $VOLUNTARYCHECK`
@@ -248,7 +190,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by CRUEL M.KH                 \e[00;37;40m"
+echo -e "               \e[01;34m                    by shield team     \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -263,7 +205,7 @@ sleep 1.3
 # Checking if the bot folder is in HOME
 echo -e "$bld$f4 CHECKING INSTALLED BOT...$rst"
 sleep 0.5
-ls ../ | grep $BOT > /dev/null
+ls ../ | grep $BOT 2>/dev/null >/dev/null
 if [ $? != 0 ]; then
   echo -e "$f1 ERROR: BOT: $BOT NOT FOUND IN YOUR HOME DIRECTORY$rst"
   sleep 4
@@ -274,11 +216,12 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m                                              \033[0;00m"
+echo -e "\033[38;5;208m        shield team                           \033[0;00m"
+echo -e "\033[38;5;208m          @shieldTM                           \033[0;00m"
+echo -e "\033[38;5;208m          developer : @hafez1116hafez         \033[0;00m"
+echo -e "\033[38;5;208m                                              \033[0;00m"
+
 # Starting preliminar setup
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -321,7 +264,7 @@ done
 # I had some weird errors, so I had to do this silly fix:
 SCREENPID1=`cat SC1`
 SCREENPID2=`cat SC2`
-rm SC1 SC2 >/dev/null
+rm SC1 SC2 2>/dev/null
 
 sleep 0.7
 CLIPID=`ps -e | grep telegram-cli | sed 's/^[[:space:]]*//' | cut -f 1 -d" "`
@@ -347,7 +290,7 @@ sleep 1
 # Opening new screen in a daemon
 echo -e "$bld$f4 ATTACHING SCREEN AS DAEMON...$rst"
 # Better to clear cli status before
-rm ../.telegram-cli/state  > /dev/null 
+rm ../.telegram-cli/state 2>/dev/null
 screen -d -m bash launch.sh
 
 sleep 1.3
@@ -401,11 +344,21 @@ sleep 5
 	echo ""
 	
 	cat /proc/$CLIPID/task/$CLIPID/status > CHECK
+	if [ $? != 0 ]; then
+		I=$(( $I + 1 ))
+		if [ $I -ge 3 ]; then
+			rm ../.telegram-cli/state 2>/dev/null
+			NONVOLUNTARY=0
+			NONVOLUNTARYCHECK=0
+			VOLUNTARY=0
+			VOLUNTARYCHECK=0
+		fi
+	else
+		I=1
+	fi
 	VOLUNTARYCHECK=`grep voluntary CHECK | head -1 | cut -f 2 -d":" | sed 's/^[[:space:]]*//'`
 	NONVOLUNTARYCHECK=`grep nonvoluntary CHECK | cut -f 2 -d":" | sed 's/^[[:space:]]*//'`
-	#echo -e "NONVOLUNTARYCHECK CTXT SWITCHES: $NONVOLUNTARYCHECK"
-	#echo -e "NONVOLUNTARY CTXT SWITCHES: $NONVOLUNTARY"
-	
+
 	if [ $NONVOLUNTARY != $NONVOLUNTARYCHECK ] || [ $VOLUNTARY != $VOLUNTARYCHECK ]; then
 		echo -e "$f5 BOT RUNNING!$rst"
 		OK=$(( $OK + 1 ))
@@ -415,7 +368,7 @@ sleep 5
 		BAD=$(( $BAD + 1 ))
 		sleep 1
 		
-		rm ../.telegram-cli/state  > /dev/null 
+		rm ../.telegram-cli/state 2>/dev/null
 
 		kill $CLIPID
 		kill $SCREEN
@@ -446,6 +399,12 @@ sleep 5
 		
 	fi
 	
+	# Clear cache after 10h
+	if [ "$OK" == 2400 ]; then
+		sync
+		sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+	fi
+	
 	VOLUNTARY=`echo $VOLUNTARYCHECK`
 	NONVOLUNTARY=`echo $NONVOLUNTARYCHECK`
 	sleep $RELOADTIME
@@ -464,7 +423,7 @@ echo "Bot running in the backgroud with TMUX"
 echo ""
 echo -e "\e[0m"
 sleep 3
-tmux kill-session script
+tmux kill-session script 2>/dev/null
 exit 1
 }
 
@@ -540,15 +499,14 @@ while getopts ":tsTSih" opt; do
 	i)
 	echo -e "\e[1m"
 	echo -e ""
-	echo "steady.sh bash script v1 CRUEL 2016 GPMOD" >&2
+	echo "steady.sh bash script v1.2 iicc 2016 DBTeam" >&2
 	echo ""
 	echo -e "\e[0m"
-
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m                                            \033[0;00m"
+echo -e "\033[38;5;208m        shield team                         \033[0;00m"
+echo -e "\033[38;5;208m         @shieldTM                          \033[0;00m"
+echo -e "\033[38;5;208m        developer : @hafez1116hafez\033[0;00m"
+echo -e "\033[38;5;208m                                              \033[0;00m"	
 echo ""
 	exit 1
       ;;
@@ -590,4 +548,5 @@ echo ""
       ;;
   esac
 done
+
 
