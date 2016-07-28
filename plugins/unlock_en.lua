084ebf4e904bc922d358f4ed0938a759ab521b73
shereyderam <sororoshervin@gmail.com>
2016-07-28 17:03:33 +0430

    Create unlock_en.lua

---
+++ plugins/unlock_en.lua
@@ -0,0 +1,73 @@
+do
+ function run(msg, matches)
+return [[ 
+UnLockHelp:
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock links
+💥Open Link Ads
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock flood
+💥Open Flood Sending
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock arabic
+💥Open Arabic/Persia
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock member
+💥Open MemeBer Adder
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock english
+💥Open English
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock sticker
+💥️Open SendSticker
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock contacts
+💥Open Contacts PosT
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock strict
+💥️Hard Settings:No
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock tgservice
+💥️Open TgService
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock fwd
+💥️Open Forwarding Post
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock reply
+💥️Open Reply
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock fosh
+💥️Open BadWord
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock leave
+💥Open Rejoin
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock operator
+💥️Open Sharj Ads
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock tag
+💥️Open HashTag #
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock emoji
+💥Open Send Emoji
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock username
+💥️Open @ Word
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock join
+💥Open JoinGroup
+〰〰〰〰〰〰〰〰〰〰〰
+#unlock media
+💥️Open Send All Media
+〰〰〰〰〰〰〰〰〰〰〰
+⚠️End UnLockHelp⚠
+]]
+end
+return {
+patterns = {
+"^[!/#][Uu]nlock en$",
+"^[Uu]nlock en$"
+},
+run = run
+}
+end
