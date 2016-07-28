fef7e41c1cb3cb0b27ed0acafe9d399e396a76fd
shereyderam <sororoshervin@gmail.com>
2016-07-28 16:46:34 +0430

    Create mute_en.lua

---
+++ plugins/mute_en.lua
@@ -0,0 +1,53 @@
+do
+ function run(msg, matches)
+return [[ 
+💥MuteHelp💥:
+〰〰〰〰〰〰〰〰〰
+#mute gifs
+💥Close Send Gifs
+〰〰〰〰〰〰〰〰〰
+#mute audio
+💥Close Send Audio
+〰〰〰〰〰〰〰〰〰
+#mute video
+💥Close Send Video
+〰〰〰〰〰〰〰〰〰
+#mute photo
+💥Close Send Photo
+〰〰〰〰〰〰〰〰〰
+#mute all
+💥️Silent Group
+〰〰〰〰〰〰〰〰〰
+💥️UnMuteHelp💥️:
+〰〰〰〰〰〰〰〰〰
+#unmute gifs
+💥️Open Send Gifs
+〰〰〰〰〰〰〰〰
+#unmute audio
+💥️Open Send Audio
+〰〰〰〰〰〰〰〰〰
+#unmute video
+💥️Open Send Video
+〰〰〰〰〰〰〰〰〰
+#unmute photo
+💥️Open Send Photo
+〰〰〰〰〰〰〰〰〰
+#unmute all
+💥UnSilent Group
+〰〰〰〰〰〰〰〰〰
+Other Command:
+#mutelist
+💥Show Silent User
+〰〰〰〰〰〰〰〰〰
+@cliTEAM
+〰〰〰〰〰〰〰〰〰
+]]
+end
+return {
+patterns = {
+"^[!/#][Mm]ute en$",
+"^[Mm]ute en$"
+},
+run = run
+}
+end
