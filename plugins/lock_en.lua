aebc0b1613b1c5d2fa9f846bc2446336266acc78
shereyderam <sororoshervin@gmail.com>
2016-07-28 16:57:52 +0430

    Create lock_en.lua

---
+++ plugins/lock_en.lua
@@ -0,0 +1,73 @@
+do
+ function run(msg, matches)
+return [[ 
+LockHelp:
+〰〰〰〰〰〰〰〰〰〰〰
+#lock links
+💥Close Link Ads
+〰〰〰〰〰〰〰〰〰〰〰
+#lock flood
+💥Close Flood Sending
+〰〰〰〰〰〰〰〰〰〰〰
+#lock arabic
+💥Close Arabic/Persia
+〰〰〰〰〰〰〰〰〰〰〰
+#lock member
+💥Close MemeBerAdder
+〰〰〰〰〰〰〰〰〰〰〰
+#lock english
+💥Close English
+〰〰〰〰〰〰〰〰〰〰〰
+#lock sticker
+💥️Close Send Sticker
+〰〰〰〰〰〰〰〰〰〰〰
+#lock contacts
+💥️Close Contacts PosT
+〰〰〰〰〰〰〰〰〰〰〰
+#lock strict
+💥️Hard Settings:Yes
+〰〰〰〰〰〰〰〰〰〰〰
+#lock tgservice
+💥️Close TgService
+〰〰〰〰〰〰〰〰〰〰〰
+#lock fwd
+💥️Close Forwarding Post
+〰〰〰〰〰〰〰〰〰〰〰
+#lock reply
+💥️Close Reply
+〰〰〰〰〰〰〰〰〰〰〰
+#lock fosh
+💥️Close BadWord
+〰〰〰〰〰〰〰〰〰〰〰
+#lock leave
+💥️Close Rejoin
+〰〰〰〰〰〰〰〰〰〰〰
+#lock operator
+💥️Close Sharj Ads
+〰〰〰〰〰〰〰〰〰〰〰
+#lock tag
+💥️Close HashTag #
+〰〰〰〰〰〰〰〰〰〰〰
+#lock emoji
+💥️Close Send Emoji
+〰〰〰〰〰〰〰〰〰〰〰
+#lock username
+💥️Close @ Word
+〰〰〰〰〰〰〰〰〰〰〰
+#lock join
+💥️Close JoinGroup
+〰〰〰〰〰〰〰〰〰〰〰
+#lock media
+💥️Close Send All Media
+〰〰〰〰〰〰〰〰〰〰〰
+@CLITEAM
+]]
+end
+return {
+patterns = {
+"^[!/#][Ll]ock en$",
+"^[Ll]ock en$"
+},
+run = run
+}
+end
