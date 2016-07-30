fd220e96931e7c4fa9d4279dc8aa4feb6afbb0d7
shereyderam <sororoshervin@gmail.com>
2016-07-30 20:40:45 +0430

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, "✅", ok_cb, false)
+		return "💢انجام شد💢"
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
